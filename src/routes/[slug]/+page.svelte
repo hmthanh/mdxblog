e615f0570ea700ce6bed3c7a6892deaa0be323c2
Hoang Minh Thanh <hmthanhgm@gmail.com>
2024-02-03 15:36:04 +0700

    export icons

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,17 +1,19 @@
 <script>
-  import { browser } from "$app/environment";
-  import BlogPost from "$lib/components/BlogPost.svelte";
-  import lazyload from "vanilla-lazyload";
+  import { browser } from "$app/environment"
+  import BlogPost from "$lib/components/BlogPost.svelte"
+  import lazyload from "vanilla-lazyload"
+  import { ArrowRightIcon } from "$lib/icons"
 
   /** @type {import('./$types').PageData} */
-  export let data;
+  export let data
 
-  const { page, post, imageData } = data;
+  const { page, post, imageData } = data
 
   if (browser && !document.lazyloadInstance) {
-    document.lazyloadInstance = new lazyload();
+    document.lazyloadInstance = new lazyload()
   }
 </script>
 
 <BlogPost {post} {imageData} />
+
 <svelte:component this={page} />
