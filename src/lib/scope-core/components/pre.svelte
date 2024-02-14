d046504cd2cdf179f9d86f43e3d7f114c750ea59
Hoang Minh Thanh <hmthanhgm@gmail.com>
2024-02-14 09:31:04 +0700

    update

---
+++ src/lib/scope-core/components/pre.svelte
@@ -0,0 +1,10 @@
+<script>
+  import cn from "clsx"
+
+  let _class = ""
+  export { _class as class }
+</script>
+
+<div class={cn("not-prose", _class)}>
+  <Pre {...$$restProps}><slot /></Pre>
+</div>
